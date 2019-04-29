3bdb1e6aa26694bf112cdd649f14da2bfeff9527
Mir Nawaz <inbox.mirnawaz@gmail.com>
2019-04-29 23:39:46 +0500

    Updates

---
+++ precache-manifest.82ffa52902fc5a2894aa61b260b5a84e.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-inspinia/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "15f483ff5139078f1a16",
+    "url": "/react-inspinia/static/js/0.fd40c2d1.chunk.js"
+  },
+  {
+    "revision": "0aeace979bb77469c1b9",
+    "url": "/react-inspinia/static/js/main.dfc8b621.chunk.js"
+  },
+  {
+    "revision": "05b16ef8c4ecd5331867",
+    "url": "/react-inspinia/static/js/runtime~main.7d138ab7.js"
+  },
+  {
+    "revision": "26dbf0a354c3b2027c2a",
+    "url": "/react-inspinia/static/js/3.e1783f0c.chunk.js"
+  },
+  {
+    "revision": "33950b1989f24f88a3894f0ce3109f47",
+    "url": "/react-inspinia/static/media/3.33950b19.jpg"
+  },
+  {
+    "revision": "39d7fce07ef9980ed88e",
+    "url": "/react-inspinia/static/js/4.8c3506fc.chunk.js"
+  },
+  {
+    "revision": "dd5c23469d5041758ba1e774e91cce0f",
+    "url": "/react-inspinia/static/media/profile.dd5c2346.jpg"
+  },
+  {
+    "revision": "eddd1a024b7afc6b7a09",
+    "url": "/react-inspinia/static/js/5.69cba0cd.chunk.js"
+  },
+  {
+    "revision": "9c71d5dd4ca8a2f70c20",
+    "url": "/react-inspinia/static/js/6.81add1cb.chunk.js"
+  },
+  {
+    "revision": "209fce86b853f393d4ad",
+    "url": "/react-inspinia/static/js/7.322af1eb.chunk.js"
+  },
+  {
+    "revision": "aa9d2f60365938360546",
+    "url": "/react-inspinia/static/js/8.8937ea03.chunk.js"
+  },
+  {
+    "revision": "f73d6cff2c0c0e648d63",
+    "url": "/react-inspinia/static/js/9.53374a7e.chunk.js"
+  },
+  {
+    "revision": "5e873241a8be87b5c6d2",
+    "url": "/react-inspinia/static/js/10.5937d46c.chunk.js"
+  },
+  {
+    "revision": "3b499bb4ad7f8239910e",
+    "url": "/react-inspinia/static/js/11.6eaab13c.chunk.js"
+  },
+  {
+    "revision": "6d4f24a593709a8a22eb",
+    "url": "/react-inspinia/static/js/12.14dc649e.chunk.js"
+  },
+  {
+    "revision": "7680cd9007b45828dc2a",
+    "url": "/react-inspinia/static/js/13.78afed14.chunk.js"
+  },
+  {
+    "revision": "90a01ee5295a83b6f1d2",
+    "url": "/react-inspinia/static/js/14.3ba925d5.chunk.js"
+  },
+  {
+    "revision": "a0b9a47b5913e1f90310",
+    "url": "/react-inspinia/static/js/15.87827842.chunk.js"
+  },
+  {
+    "revision": "55f64bff2190ee81e8ff73bc98f5a444",
+    "url": "/react-inspinia/static/media/landing_2.55f64bff.jpg"
+  },
+  {
+    "revision": "3a721b6bc24e35e0d4cd291c9c1af167",
+    "url": "/react-inspinia/static/media/landing_3.3a721b6b.jpg"
+  },
+  {
+    "revision": "21cc514114129d3f0a2d976d2627cc9d",
+    "url": "/react-inspinia/static/media/logo.21cc5141.png"
+  },
+  {
+    "revision": "4ed3451730c199bc4fa64d0d8d516a7b",
+    "url": "/react-inspinia/static/media/a2.4ed34517.jpg"
+  },
+  {
+    "revision": "c2faf8a52c6b42a4d32f4f428aabb7c9",
+    "url": "/react-inspinia/static/media/landing_1.c2faf8a5.jpg"
+  },
+  {
+    "revision": "b689816501876d1d1d78e22fc9bfa866",
+    "url": "/react-inspinia/static/media/landing_4.b6898165.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-inspinia/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "dc339f2294c01abd464c50c8fda4b3c4",
+    "url": "/react-inspinia/static/media/a7.dc339f22.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-inspinia/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-inspinia/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-inspinia/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "dd77cc9a389082233e62920ec8b28c40",
+    "url": "/react-inspinia/static/media/4.dd77cc9a.png"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react-inspinia/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react-inspinia/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react-inspinia/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "85efa900c0fc12fee15a5398deba06e8",
+    "url": "/react-inspinia/static/media/header-profile-skin-1.85efa900.png"
+  },
+  {
+    "revision": "8307c45ca34d4af71912b535b6c05c54",
+    "url": "/react-inspinia/static/media/header-profile-skin-2.8307c45c.png"
+  },
+  {
+    "revision": "bf471ec3d4085883e061ca35006e86e8",
+    "url": "/react-inspinia/static/media/header-profile-skin-3.bf471ec3.png"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react-inspinia/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "925633f6715929c667b2541eb7dad40e",
+    "url": "/react-inspinia/static/media/word_map.925633f6.png"
+  },
+  {
+    "revision": "1139d7d86ba8ed59032af821904b180a",
+    "url": "/react-inspinia/static/media/header_one.1139d7d8.jpg"
+  },
+  {
+    "revision": "f721466883998665b87923b92dea655b",
+    "url": "/react-inspinia/static/media/glyphicons-halflings-regular.f7214668.svg"
+  },
+  {
+    "revision": "ea2316224d45899c59bc285ba09dd920",
+    "url": "/react-inspinia/static/media/shattered.ea231622.png"
+  },
+  {
+    "revision": "c5a509acee8aab3a1f62e16ea1204ffc",
+    "url": "/react-inspinia/static/media/avatar_all.c5a509ac.png"
+  },
+  {
+    "revision": "f2f2de96e14a40255e93ab5fab29916b",
+    "url": "/react-inspinia/static/media/header_two.f2f2de96.jpg"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/react-inspinia/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/react-inspinia/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/react-inspinia/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/react-inspinia/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "0aeace979bb77469c1b9",
+    "url": "/react-inspinia/static/css/main.5092c4d6.chunk.css"
+  },
+  {
+    "revision": "eddd1a024b7afc6b7a09",
+    "url": "/react-inspinia/static/css/5.3fd26b1c.chunk.css"
+  },
+  {
+    "revision": "39d7fce07ef9980ed88e",
+    "url": "/react-inspinia/static/css/4.d221e1de.chunk.css"
+  },
+  {
+    "revision": "f614bf579b12396a646c91d24d3a775c",
+    "url": "/react-inspinia/index.html"
+  }
+];
